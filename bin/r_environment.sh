a30caa558bd287a7ca4a8f5c20f89c11b794396c
Kagoshima <kagosa@getgkagosam1.corp.emc.com>
2013-11-26 18:21:00 +0100

    environment variable checking

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -2,6 +2,6 @@
 
 GCC_VERSION="4.3"
 
-export R_HOME=/app/vendor/R
+export R_HOME=/tmp/staged/app/vendor/R
 export R_INCLUDE=$R_HOME/lib64/R/include
-export PATH=$R_HOME/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+export PATH=$R_HOME/bin:/tmp/staged/app/vendor/gcc-$GCC_VERSION/bin:$PATH
