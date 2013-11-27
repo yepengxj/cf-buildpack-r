654aae340be937692904d3e512bcc75de847f2ea
Kagoshima <kagosa@getgkagosam1.corp.emc.com>
2013-11-27 17:38:17 +0100

    compiling R instead of binary

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-GCC_VERSION="4.3"
-
-export R_HOME=/tmp/staged/app/app/vendor/R
+export R_HOME=/tmp/staged/app/R
 export R_INCLUDE=$R_HOME/lib64/R/include
-export PATH=$R_HOME/bin:/tmp/staged/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+export PATH=$R_HOME/bin:$PATH
