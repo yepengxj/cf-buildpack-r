9a213b0a6d5b582a59aa10ce0e7de4e520046af1
yepengxj <yepeng_xj@163.com>
2015-08-03 22:18:32 +0800

    modified:   bin/compile 	modified:   bin/r_environment.sh

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -5,5 +5,5 @@ GCC_VERSION="4.3"
 export R_HOME=/app/vendor/R
 export R_INCLUDE=$R_HOME/lib64/R/include
 export PATH=$R_HOME/bin:$PATH
-export LD_LIBRARY_PATH="/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:$R_HOME/lib64:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:$R_HOME/lib64:$LD_LIBRARY_PATH"
